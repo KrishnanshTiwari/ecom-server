e1230bf68c3b35b261d47c4ff1c816d106e19208
Krishnansh Tiwari <krihnansht2003@gmail.com>
2024-02-04 19:11:18 +0530

    mongo connected

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = (url) => {
+    mongoose.connect(url)
+    .then(()=>console.log('connceted to mongo'))
+    .catch((err)=>{
+        console.error('Failed to connected mongo');
+        console.error(err);
+    })
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+const mongoURI = process.env.mongoURI;
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+
 // Define a simple route
-app.get('/',(req,res)=>{
-    res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
-// Start the server
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`);
-});
+// Start the server and connect to mongodb
+const startServer = async () => {
+  try {
+    console.log(mongoURI);
+    connectDB(mongoURI);
+    app.listen(8080, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
